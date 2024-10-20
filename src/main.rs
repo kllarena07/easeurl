e3bb30fd2c48ab10b5d08a422baf2e56bdd4c8fb
Kieran Llarena <baymax709@gmail.com>
2024-10-20 12:58:25 -0400

    feat: serve html on port 3000

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_files::NamedFile;
+use actix_web::{get, App, HttpServer, Result};
+use std::env;
+use std::path::PathBuf;
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> Result<NamedFile> {
+    let root = env::current_dir().unwrap();
+    let abs_path = format!("{}/app/index.html", root.to_str().unwrap());
+    let path: PathBuf = PathBuf::from(abs_path);
+    Ok(NamedFile::open(path)?)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Listening on 127.0.0.1:3000");
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(index)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 3000))?
     .run()
     .await
 }
